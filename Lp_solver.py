c7a98b0e1b3c9c7482014139d551750b90b40edd
Alvaro Arenas <alvaare@gmail.com>
2020-02-12 11:51:11 +0100

    cleaning prints

--- Lp_solver.py
+++ Lp_solver.py
@@ -3,7 +3,7 @@ import numpy as np
 import numpy.random as rd
 import queue
 
-n = 6
+n = 10
 s,t = 0, n-1
 
 def rand_graph():
--- lib/matroid_intersection.cpp
+++ lib/matroid_intersection.cpp
@@ -120,21 +120,14 @@ bool can_add(d_node e, part_m* M) {
 }
 
 void add_graphic_m(d_node e, graphic_m* M) {
-  cout << "Call add_graphic_m\n";
   set<node>* s1 = M->set_of_node[e.i][e.id_k];
   set<node>* s2 = M->set_of_node[e.j][e.id_k];
   set<node>* s = new set<node>;
   *s = union_set(s1, s2);
-  print_d_node(e);
-  cout << "\n";
-  cout << s1 << "\n";
-  cout << s2 << "\n";
-  print_graphic_m(M);
   delete s1;
   delete s2;
   for(set<node>::iterator it=s->begin(); it!=s->end(); it++)
     M->set_of_node[it->id][it->id_k] = s;
-  cout << "End add_graphic_m\n";
 }
 
 void add_part_m(d_node e, part_m* M) {
@@ -182,22 +175,15 @@ void split_set(d_node e,ind_set* J,set<node>* s, set<node>* s1, set<node>* s2) {
 }
 
 void erase_graphic_m(d_node e, graphic_m* M, ind_set* J) {
-  cout << "Call erase_graphic_m\n";
-  print_ind_set(J);
   set<node>* s = M->set_of_node[e.i][e.id_k];
-  print_set_node(s);
   set<node>* si = new set<node>;
   set<node>* sj = new set<node>;
   split_set(e, J, s, si, sj);
-  print_set_node(s);
-  print_set_node(si);
-  print_set_node(sj);
   delete s;
   for(set<node>::iterator it=si->begin(); it!=si->end(); it++)
     M->set_of_node[it->id][it->id_k] = si;
   for(set<node>::iterator it=sj->begin(); it!=sj->end(); it++)
     M->set_of_node[it->id][it->id_k] = sj;
-  cout << "End erase_graphic_m\n";
 }
 
 void erase_part_m(d_node e, part_m* M) {
@@ -205,42 +191,27 @@ void erase_part_m(d_node e, part_m* M) {
 }
 
 bool can_exchange(d_node e, d_node f, graphic_m* M, ind_set* J) {
-  cout << "Call can_exchange1\n";
-  print_graphic_m(M);
-  print_d_node(e);
-  cout << "\n";
-  print_d_node(f);
-  cout << "\n";
   if (J->find(f)!=J->end())
     erase_graphic_m(f, M, J);
-  cout << "_______________________________________________________________\n";
-  print_graphic_m(M);
   bool res = can_add(e, M);
   if (J->find(f)!=J->end())
     add_graphic_m(f, M);
-  cout << "End can_exchange1\n";
   return res;
 }
 
 bool can_exchange(d_node e, d_node f, part_m* M, ind_set* J) {
-  cout << "Call can_exchange2\n";
   if (J->find(f)!=J->end())
     erase_part_m(f, M);
   bool res = can_add(e, M);
   if (J->find(f)!=J->end())
     add_part_m(f, M);
-  cout << "End can_exchange2\n";
   return res;
 }
 
 digraph const_digraph(graphic_m* M1, part_m* M2, ind_set* J, graph* G, int k) {
-  cout << "Call const_digraph\n";
   digraph DG = initialize_digraph(G, k);
-  print_digraph(&DG);
   for (digraph::iterator it=DG.begin(); it!=DG.end(); ++it) {
     d_node e = it->first;
-    print_d_node(e);
-    cout << "\n";
     if (e!=s() && e!=r() && J->find(e)==J->end()) {
       if (can_add(e, M1))
         DG[e].push_back(s());
@@ -248,13 +219,10 @@ digraph const_digraph(graphic_m* M1, part_m* M2, ind_set* J, graph* G, int k) {
         DG[r()].push_back(e);
       for(ind_set::iterator it=J->begin(); it!=J->end(); it++) {
         d_node f = *it;
-        cout << "HOLA\n";
         if (can_add(e, M1) && can_exchange(e, f, M1, J))
           DG[e].push_back(f);
-        cout << "BUENAS\n";
         if (can_add(e, M2) && can_exchange(e, f, M2, J))
           DG[f].push_back(e);
-        cout << "ADIOS\n";
       }
     }
   }
@@ -278,26 +246,17 @@ void print_father(map<d_node, d_node> father) {
 }
 
 dipath dipath_from_fathers(map<d_node,d_node> father) {
-  print_father(father);
   dipath P;
   d_node v = s();
   while(v!=r()) {
-    if (v!=s()) {
-    print_d_node(v);
-    cout << "\n"; }
     if (father.find(v)==father.end()) {
       dipath empty;
-      cout << "EMPTY\n";
       return empty;
-    }
-    cout << "ENTRE\n";
-    print_d_node(father[v]);
-    cout << "\n";
+    };
     v = father[v];
     P.push_back(v);
   }
   P.pop_back();
-  cout << "Call to return_dipath\n";
   return return_dipath(P);
 }
 
@@ -323,12 +282,10 @@ dipath find_dipath(digraph* G) {
         Q.push(neighbors[i]);
       }
   }
-  cout << "Call to dipath_from_fathers\n";
   return dipath_from_fathers(father);
 }
 
 void update_ind_set(ind_set* J, graphic_m* M1, part_m* M2, dipath P) {
-  cout << "Call to update_ind_set\n";
   int i = 0;
   for (; i<(int)P.size()-1; i+=2) {
     erase_part_m(P[i+1], M2);
@@ -338,11 +295,9 @@ void update_ind_set(ind_set* J, graphic_m* M1, part_m* M2, dipath P) {
     J->insert(P[i]);
     J->erase(P[i+1]);
   }
-  cout << "End for\n";
   J->insert(P[i]);
   add_part_m(P[i], M2);
   add_graphic_m(P[i], M1);
-  cout << "End of update_ind_set\n";
 }
 
 void print_ind_set(ind_set* A) {
@@ -359,20 +314,12 @@ ind_set max_ind_set(graph* G, int k) {
   ind_set J;
   graphic_m M1(n,k);
   part_m M2(n,k,G);
+  int i=0;
   while(true) {
-    cout << "Begin while\n";
     digraph DG = const_digraph(&M1, &M2, &J, G, k);
-    cout << "End of const_digraph\n";
-    print_digraph(&DG);
     dipath P = find_dipath(&DG);
-    cout << "end of find_dipath\n";
-    print_dipath(&P);
-    cout << "ALMOST\n";
-    if (P.empty()) {
-      cout << "REALLY EMPTY\n";
-      print_ind_set(&J);
+    if (P.empty())
       return J;
-    }
     else
       update_ind_set(&J, &M1, &M2, P);
   }
